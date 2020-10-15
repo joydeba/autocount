f2333daa4f7211528241ccbc820bbb3dc74e7bbd
Ubuntu <ubuntu@joysdeepimg.arbutus>
2020-10-15 08:59:10 +0000

    BSR done

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import csv
 import json
 from pprint import pprint
 import collections
+import scipy.io
+import glob
 
 def data_grouping_GWHD():
     result = {}
@@ -49,6 +51,25 @@ def data_grouping_COCO():
             writer.writerow([key, value])
             i = i + 1
 
-data_grouping_COCO()    
-data_grouping_GWHD()
+def data_grouping_BSR():
+    result_count = {}
+    matsloc = glob.glob("BSRtrain200_fromBSD500/*.mat")
+    for matloc in matsloc:
+        mat = scipy.io.loadmat(matloc)
+        # print(len(mat['groundTruth'][0]))
+        if len(mat['groundTruth'][0]) in result_count:
+            result_count[len(mat['groundTruth'][0])] = result_count[len(mat['groundTruth'][0])] + 1
+        else:
+            result_count[len(mat['groundTruth'][0])] = 1
 
+    result_count = collections.OrderedDict(sorted(result_count.items()))
+    with open('BSR_distribution.csv', 'w') as csv_file:  
+        writer = csv.writer(csv_file)
+        i = 1
+        for key, value in result_count.items():
+            writer.writerow([key, value])
+            i = i + 1        
+
+# data_grouping_COCO()    
+# data_grouping_GWHD()
+data_grouping_BSR()

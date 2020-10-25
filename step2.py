b43736b3a96c4b29ae0dc7284bb6c2b1d7df9f96
Ubuntu <ubuntu@joysdeepimg.arbutus>
2020-10-25 08:33:55 +0000

    Documentation Done

--- step2.py
+++ step2.py
@@ -19,12 +19,12 @@ input_file = sys.argv[1]
 num_threads = int(sys.argv[3])
 labels_file = sys.argv[2]
 
-img_dict = load(input_file)
+img_dict = load(input_file) # Segmented input images from dict
 num_samples = len(img_dict)
 
 
 
-
+# never used
 def csv_to_dict(filename):
     ret = {}
 
@@ -36,7 +36,7 @@ def csv_to_dict(filename):
 
     return ret
 
-
+# never used
 def show_performance_stats(predicted, all_y, labels, params):
     for i in range(len(labels)):
         print('{0}: {1} {2} {3}'.format(labels[i], all_y[i], predicted[i], params[i]))
@@ -66,17 +66,17 @@ def show_performance_stats(predicted, all_y, labels, params):
 
 
 def do_detection(v, gauss, d_diam, e_diam, thresh):
-    v2 = gaussian(v, gauss)
+    v2 = gaussian(v, gauss) #Multi-dimensional Gaussian filter to detect edges.
 
-    image = np.zeros_like(v2)
-    image[v2 >= thresh] = 1.
-
+    image = np.zeros_like(v2) # Return an array of zeros with the same shape and type as a given array
+    image[v2 >= thresh] = 1. 
+    # label to define unique objects, finds the convex hull of each using convex_hull_image 
     image = morph.dilation(image, morph.disk(d_diam))
     image = morph.erosion(image, morph.disk(e_diam))
 
     return image
 
-
+# usses watershed 
 def do_segmentation(v, diam, d_diam):
     v = remove_small_objects(v > 0., d_diam)
 
@@ -102,11 +102,11 @@ def get_param_count(img, diam, d_diam2):
 
     image = np.pad(img, mode='constant', pad_width=100)
 
-    labels = do_segmentation(image, diam, d_diam2)
+    labels = do_segmentation(image, diam, d_diam2) # with watershed
 
     return len(regionprops(labels))
 
-
+# Detect optimal params from loss
 def find_detection_params_for_image(v):
     cache = {}
 
@@ -116,9 +116,9 @@ def find_detection_params_for_image(v):
         if params in cache.keys():
             return cache[params]
 
-        image = do_detection(v, gauss, d_diam, e_diam, thresh)
+        image = do_detection(v, gauss, d_diam, e_diam, thresh) # return object edges
 
-        prop = regionprops(label(image))
+        prop = regionprops(label(image)) # regionprops() result to draw certain properties on each region.
         c1 = np.array([p.area for p in prop], dtype=np.float32)
 
         if len(c1) < 3:
@@ -134,22 +134,22 @@ def find_detection_params_for_image(v):
         cache[params] = image_loss
 
         return image_loss
-
+    # Instrumentation helps you convert a set of arguments into variables in the data space which can be optimized
     bounds = ng.Instrumentation(ng.var.Scalar(float).bounded(0., 10.),
                                 ng.var.Scalar(int).bounded(0, 10),
                                 ng.var.Scalar(int).bounded(0, 10),
                                 ng.var.Scalar(float).bounded(0., 1.))
-
+    # is excellent in discrete settings of mixed settings when high precision on parameters is not relevant; it's possibly a good choice for hyperparameter choice.
     optimizer = ng.optimizers.PortfolioDiscreteOnePlusOne(instrumentation=bounds, budget=4000)
-
+    # Hyper parameter for detection loss and detection
     recommendation = optimizer.minimize(detection_loss)
-    opt_params = recommendation.args
+    opt_params = recommendation.args # optimal params from loss
 
-    return opt_params
+    return opt_params # params 
 
-
+# optimized params for all images 
 def find_segmentation_params_for_all_images(images):
-    def linear_search(loss_fn, range_a, range_b):
+    def linear_search(loss_fn, range_a, range_b): #linear search
         opt_diam = None
         opt_ddiam = None
         opt_loss = None
@@ -165,9 +165,9 @@ def find_segmentation_params_for_all_images(images):
 
         return opt_diam, opt_ddiam
 
-    def segmentation_loss(diam, d_diam):
+    def segmentation_loss(diam, d_diam): 
         def par_loop(v, diam, d_diam):
-            def wasserstein(dist1, dist2):
+            def wasserstein(dist1, dist2): # distance 
                 bins = np.arange(min(dist1.ppf(0.01), dist2.ppf(0.01)), max(dist1.ppf(0.99), dist2.ppf(0.99)))
                 return np.sum(np.abs(dist1.cdf(bins) - dist2.cdf(bins)))
 
@@ -235,7 +235,7 @@ def find_segmentation_params_for_all_images(images):
 
     opt_params = linear_search(segmentation_loss, range(1, 20), range(0, 10))
 
-    return opt_params
+    return opt_params # optimal params based on linear search + seg loss (wasserstein loss)
 
 
 # Find all optimal detection parameterizations
@@ -248,16 +248,16 @@ params_dict = {}
 for f, m in img_dict.items():
     filenames.append(f)
     mats.append(m)
-
+# Mats are paralleled for finding optimal parameters from losses 
 all_detect_params = Parallel(n_jobs=num_threads)(delayed(find_detection_params_for_image)(v) for v in mats)
 
 # Find all object counts
 print('Doing detections...')
-
+# Using optimized params for object edge detection
 all_detected_mats = Parallel(n_jobs=num_threads)(delayed(do_detection)(v, p[0], p[1], p[2], p[3]) for v, p in zip(mats, all_detect_params))
 
 print('Doing global segmentation optimization...')
-
+# Finding optimized params for all together
 seg_params = find_segmentation_params_for_all_images(all_detected_mats)
 print('Segmentation parameters found: ')
 print(seg_params)
@@ -265,7 +265,7 @@ print(seg_params)
 print('Counting objects...')
 
 for f, m, opt_params in zip(filenames, all_detected_mats, all_detect_params):
-    c = get_param_count(m, seg_params[0], seg_params[1])
+    c = get_param_count(m, seg_params[0], seg_params[1]) # count using watershed segmentation
 
     counts_dict[f] = c
     params_dict[f] = list(opt_params)

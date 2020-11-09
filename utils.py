5f830efe2979ddd61d3216cdbaf59b70979e87dc
Ubuntu <ubuntu@joysdeepimg.arbutus>
2020-11-09 08:19:44 +0000

    Pixel fixation

--- utils.py
+++ utils.py
@@ -368,8 +368,23 @@ def testing_model(model, test_loader):
                 im = im.convert('RGB')
             bpixel = (0, 0)
             newcolor = (255,)*3
-            if not any(im.getpixel(bpixel)) and not any(im.getpixel(newcolor)):
-                im.putpixel(bpixel)    
+            # if not any(im.getpixel(bpixel)) and not any(im.getpixel(newcolor)):
+            #     im.putpixel(bpixel)
+            # Separate RGB arrays
+            R, G, B = im.convert('RGB').split()
+            r = R.load()
+            g = G.load()
+            b = B.load()
+            w, h = im.size
+
+            # Convert non-black pixels to white
+            for i in range(w):
+                for j in range(h):
+                    if(r[i, j] != 0 or g[i, j] != 0 or b[i, j] != 0):
+                        r[i, j] = 255 # Just change R channel
+
+            # Merge just the R channel as all channels
+            im = Image.merge('RGB', (R, R, R))    
             im.save('outfile'+str(i)+'.png')
             i = i + 1
             # ax.imshow(np.squeeze(img), cmap='gray')
